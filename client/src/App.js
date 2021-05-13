fe8689e3f0808f306968926af03a79eb085fd7c1
NivAm12 <nivamos12@gmail.com>
2021-05-13 22:23:50 +0300

    create the map component

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,20 @@ import RegisterPage from "./components/RegisterPage";
 import LoginPage from "./components/LoginPage";
 import MainPage from "./components/MainPage";
 import React from "react";
+import axios from "axios";
+import {Router, Switch} from "react-router-dom";
 
 function App() {
+
+  const getUser = async() => {
+    const response = await axios.get(`http://localhost:5000/user`);
+    console.log(response.data.user)
+    return response.data.user;
+  }
+
   return (
     <div className="App">
-      <LoginPage/>
+      {async ()=>{await getUser() != null? <MainPage/> : <LoginPage/>}}
     </div>
   );
 }
